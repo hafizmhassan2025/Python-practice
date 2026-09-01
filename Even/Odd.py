ba4250f6bfe49515cd299b153b3e5d1dd5cfdd40
Hafiz Muhammad Hassan <mhassan.tech,pk>
2026-09-01 12:52:16 +0500

    Remove wrong directory

--- Even/Odd.py
+++
@@ -1,9 +0,0 @@
-# Take input from the user
-# check whether the number is even or odd
-
-number = int(input("Enter the number."))
-
-if number%2==0:
-    print(number,"is Even.")
-else:
-    print(number,"is Odd.")
