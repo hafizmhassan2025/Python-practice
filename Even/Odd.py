282e86c4de42f9e6397aff22b2a47b2b2bf7457d
Hafiz Muhammad Hassan <mhassan.tech,pk>
2026-09-01 12:42:27 +0500

    Add Even/Odd Progarm

---
+++ Even/Odd.py
@@ -0,0 +1,9 @@
+# Take input from the user
+# check whether the number is even or odd
+
+number = int(input("Enter the number."))
+
+if number%2==0:
+    print(number,"is Even.")
+else:
+    print(number,"is Odd.")
