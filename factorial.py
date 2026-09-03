2402bcadd1a3a6c82c6c4a15eacb1859a905eef1
Hafiz Muhammad Hassan <mhassan.tech.pk>
2026-09-04 00:56:16 +0500

    Logic update

--- factorial.py
+++ factorial.py
@@ -1,9 +1,9 @@
 number = int(input("Enter the number"))
 factorial=1
 
-if number==0:
+if number==0 or number==1:
     print("Factorial = 1")
-elif number>0:
+elif number>=2:
     for i in range(1,number+1):
         factorial=factorial*i
     print("Factorial =",factorial)
