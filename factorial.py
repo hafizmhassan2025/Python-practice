c98ae729357ab17e86f4fa445cb4ec42b7c1c653
Hafiz Muhammad Hassan <mhassan.tech.pk@gmail.com>
2026-09-04 01:04:44 +0500

    Update condition

--- factorial.py
+++ factorial.py
@@ -3,7 +3,7 @@ factorial=1
 
 if number==0 or number==1:
     print("Factorial = 1")
-elif number>=2:
+elif number>1:
     for i in range(1,number+1):
         factorial=factorial*i
     print("Factorial =",factorial)
