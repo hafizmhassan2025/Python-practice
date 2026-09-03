67533d4957628342708bdc5bd0508afac06bd416
Hafiz Muhammad Hassan <mhassan.tech,pk>
2026-09-04 00:04:08 +0500

    Code of factorial

--- factorial.py
+++ factorial.py
@@ -1,1 +1,12 @@
+number = int(input("Enter the number"))
+factorial=1
 
+if number==0:
+    print("Factorial = 1")
+elif number>0:
+    for i in range(1,number+1):
+        factorial=factorial*i
+    print("Factorial =",factorial)
+else:
+    print("Undefined")        
+    
