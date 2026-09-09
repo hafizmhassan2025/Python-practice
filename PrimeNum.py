d65413e37513066a72461ec019cace8f27e9b14b
Hafiz Muhammad Hassan <mhassan.tech.pk@gmail.com>
2026-09-09 12:38:46 +0500

    Code of Prime Number Checker Program

--- PrimeNum.py
+++ PrimeNum.py
@@ -1,1 +1,16 @@
+number = int(input("Enter the number\nThe number must be greater than zero.\n"))
+Is_prime=True
 
+if number < 2:
+     Is_prime = False
+else:
+    for i in range(2,number):
+        if number%i==0:
+            Is_prime = False
+            break
+           
+if Is_prime:
+     print("The number is prime.")
+else:
+     print("The number is non-prime.")
+    
